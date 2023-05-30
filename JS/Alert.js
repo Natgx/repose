1b97c154e57351b5998de6f5eafc014850ba33d6
Natgx <102414039+Natgx@users.noreply.github.com>
2023-05-30 17:07:04 +0800

    first commit

---
+++ JS/Alert.js
@@ -0,0 +1,6 @@
+alert("Hello I am Nat's personal alert box");
+function Give_Words(){
+	var firstWord = "Hello my color is green.";
+	var changeColor = firstWord.fontcolor("green");
+	document.getElementById("Input").innerHTML = changeColor;
+}
